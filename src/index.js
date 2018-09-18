52b9b0536db46e0ac7efde7e5d7604882519ed52
Alex <alexex.by@gmail.com>
2018-09-19 01:16:22 +0300

    Implement Smart Sorter

--- src/index.js
+++ src/index.js
@@ -1,30 +1,59 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.state = [];
+    this.sortFunc;
   }
 
   add(element) {
     // your implementation
+    this.state.push(element); 
+    return this.state;
   }
 
   at(index) {
     // your implementation
+    return this.state[index];
   }
 
   get length() {
     // your implementation
+    return this.state.length;
   }
 
   toArray() {
     // your implementation
+    if (Array.isArray(this.state)) {
+      return this.state;
+    }
+    // add method to change obj to array
   }
 
   sort(indices) {
     // your implementation
+    let data = indices.map(item => this.state[item]);
+
+    function numericSort(a, b) {
+      if (b === undefined) return 1;
+      if (a > b) return 1;
+      if (a < b) return -1;
+    }
+
+    let sorterFunction = this.sortFunc === undefined ? numericSort : this.sortFunc;
+    let sortedData = data.sort(sorterFunction);
+    let newIndices = indices.sort(numericSort);
+
+    newIndices.forEach((item, index) => this.state[item] = sortedData[index]);
+    
+    return this.state;
   }
 
   setComparator(compareFunction) {
+    if (compareFunction === undefined) {
+      return false;
+    }
     // your implementation
+    return this.sortFunc = compareFunction;
   }
 }
 
